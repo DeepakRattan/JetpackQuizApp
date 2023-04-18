b884c64af3704fd7e62c5050df4fe8cc60ee4e24
Deepak Rattan <deepak.rattan@itechnolabs.tech>
2023-04-18 10:15:59 +0530

    Creates Question Tracker Composable

--- app/src/main/java/com/example/jetpackquizapp/component/Questions.kt
+++ app/src/main/java/com/example/jetpackquizapp/component/Questions.kt
@@ -3,11 +3,19 @@ package com.example.jetpackquizapp.component
 import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackquizapp.screens.QuestionsViewModel
 import com.example.jetpackquizapp.util.AppColor
 
@@ -46,7 +54,32 @@ fun QuestionDisplay() {
     }
 }
 
+@Preview
 @Composable
-fun questionTracker(){
+fun questionTracker(counter: Int = 10, outOf: Int = 100) {
+    Text(
+        text = buildAnnotatedString {
+            withStyle(style = ParagraphStyle(textIndent = TextIndent.None)) {
+                withStyle(
+                    style = SpanStyle(
+                        color = AppColor.mLightGray,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 27.sp
+                    )
+                ) {
+                    append("Question $counter/")
+                    withStyle(
+                        style = SpanStyle(
+                            color = AppColor.mLightGray,
+                            fontWeight = FontWeight.Light,
+                            fontSize = 14.sp
+                        )
+                    ) {
+                        append("$outOf")
+                    }
+                }
+            }
+        }, modifier = Modifier.padding(20.dp)
+    )
 
 }
--- app/src/main/java/com/example/jetpackquizapp/util/AppColor.kt
+++ app/src/main/java/com/example/jetpackquizapp/util/AppColor.kt
@@ -6,5 +6,13 @@ import androidx.compose.ui.graphics.Color
  * Created by Deepak Rattan on 17/04/23
  */
 object AppColor {
+    val mLightGray = Color(0xffb4b9cd)
+    val mBlack = Color(0xff060814)
+    val mOffWhite = Color(0xffeceeef)
+    val mLightPurple = Color(0xff545a75)
     val mDarkPurple = Color(0xff262C49)
+    val mOffDarkPurple = Color(0xFF22496B)
+    val mLightBlue = Color(0xff127EEA)
+    val mBrown = Color(0xff7f6749)
+    val mBlue = Color(0xff4b5f9e)
 }
